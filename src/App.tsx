384252b73b4be6ce798cc6ee77138fd8ee618957
johelder <johelder.araujo@pede.ai>
2022-09-02 16:30:01 -0300

    feat: create app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { GlobalStyles } from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles, theme } from './styles';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <h1>Hello World</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: {
+      background: '#E5E5E5',
+      light: '##F8FBFB',
+    },
+    secondary: {
+      dark_blue: '#1A2437',
+      light_blue: '#2CC3D5',
+      yellow: '#F9B42F',
+    },
+    texts: {
+      text_300: '#8CA0B3',
+      text_400: '#738192',
+      text_700: '#52636C',
+      text_800: '#56616E',
+    },
+  },
+};
