e1ae528255926c13269d80c5aed5405573d45479
johelder <johelder.araujo@pede.ai>
2022-09-02 19:26:26 -0300

    feat: adding components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 import { Header } from './Header';
+import { SortButton } from './SortButton';
+import { OrganizeListTypeButton } from './OrganizeListTypeButton';
 
-export { Header };
+export { Header, SortButton, OrganizeListTypeButton };
