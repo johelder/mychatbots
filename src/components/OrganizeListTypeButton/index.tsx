061b8056ed83e8d580912d2c0e177259516c516f
johelder <johelder.araujo@pede.ai>
2022-09-02 23:10:27 -0300

    refactor: adjust OrganizeListButton component

--- src/components/OrganizeListTypeButton/index.tsx
+++ src/components/OrganizeListTypeButton/index.tsx
@@ -3,17 +3,19 @@ import { MdViewAgenda as List } from 'react-icons/md';
 
 import * as S from './styles';
 
-interface IOrganizeListTypeButtonProps {
+export interface IOrganizeListTypeButtonProps {
   type: 'list' | 'blocks';
   isActive: boolean;
+  handleActive: () => void;
 }
 
 export const OrganizeListTypeButton = ({
   type,
   isActive,
+  handleActive,
 }: IOrganizeListTypeButtonProps) => {
   return (
-    <S.Container isActive={isActive}>
+    <S.Container isActive={isActive} onClick={handleActive}>
       {type === 'blocks' ? <Blocks /> : <List />}
     </S.Container>
   );
