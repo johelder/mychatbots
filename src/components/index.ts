aeec99f77dc31141edb285482c22dbed71b0ecfa
johelder <johelder.araujo@pede.ai>
2022-09-03 20:34:32 -0300

    refactor: delete unnecessary component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,16 +2,7 @@ import { Header } from './Header';
 import { Button } from './Button';
 import { OrganizeListTypeButton } from './OrganizeListTypeButton';
 import BotCard from './BotCard';
-import { BotCardList } from './BotCardList';
 import { Footer } from './Footer';
 import { BotInfoCard } from './BotInfoCard';
 
-export {
-  Header,
-  Button,
-  OrganizeListTypeButton,
-  BotCard,
-  BotCardList,
-  Footer,
-  BotInfoCard,
-};
+export { Header, Button, OrganizeListTypeButton, BotCard, Footer, BotInfoCard };
