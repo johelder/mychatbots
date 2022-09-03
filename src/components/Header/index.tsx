2e6860499d01ab8460c81181a3885e4bcc23501e
johelder <johelder.araujo@pede.ai>
2022-09-03 13:02:57 -0300

    refactor: adjust logo name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import Logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.svg';
 
 import * as S from './styles';
 
 export const Header = () => {
   return (
     <S.Container>
-      <img src={Logo} alt="Take Blip Logo" />
+      <img src={logo} alt="Take Blip Logo" />
     </S.Container>
   );
 };
