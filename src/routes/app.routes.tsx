a99ed6b075e0a7a53f583e009ee9d2bafca93583
johelder <johelder.araujo@pede.ai>
2022-09-03 02:04:48 -0300

    feat: adding botDetail page to index routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Dashboard } from '../pages';
+import { Dashboard, BotDetail } from '../pages';
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/:id" element={<BotDetail />} />
       </Routes>
     </Router>
   );
