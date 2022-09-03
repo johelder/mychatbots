5372bb6006c77d205c19a9b14110d96d19b41d94
johelder <johelder.araujo@pede.ai>
2022-09-03 02:02:58 -0300

    feat: adding footer to index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import { Button } from './Button';
 import { OrganizeListTypeButton } from './OrganizeListTypeButton';
 import BotCard from './BotCard';
 import { BotCardList } from './BotCardList';
+import { Footer } from './Footer';
 
-export { Header, Button, OrganizeListTypeButton, BotCard, BotCardList };
+export { Header, Button, OrganizeListTypeButton, BotCard, BotCardList, Footer };
