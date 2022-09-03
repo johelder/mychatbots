5d9bef4e560c6c9fdc517741c2568e66932ea147
johelder <johelder.araujo@pede.ai>
2022-09-02 23:08:59 -0300

    feat: adding BotCardList to index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Header } from './Header';
 import { Button } from './Button';
 import { OrganizeListTypeButton } from './OrganizeListTypeButton';
 import BotCard from './BotCard';
+import { BotCardList } from './BotCardList';
 
-export { Header, Button, OrganizeListTypeButton, BotCard };
+export { Header, Button, OrganizeListTypeButton, BotCard, BotCardList };
