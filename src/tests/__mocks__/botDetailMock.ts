76a605064c159d9c9dd1e00973f227411740134c
johelder <johelder.araujo@pede.ai>
2022-09-04 17:21:51 -0300

    test: create botDetail mock

---
+++ src/tests/__mocks__/botDetailMock.ts
@@ -0,0 +1,18 @@
+export const botDetailMock = {
+  name: 'fake-name',
+  type: 'fake-type',
+  image: 'fake-image',
+  culture: 'fake-culture',
+  analytics: {
+    user: {
+      total: 0,
+      actived: 0,
+    },
+    message: {
+      received: 0,
+      sent: 0,
+    },
+  },
+  created: 'fake-date',
+  updated: 'fake-date',
+};
