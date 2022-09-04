4a5f26aa3b22656d6bd38a7699c188d4e3025eba
johelder <johelder.araujo@pede.ai>
2022-09-04 19:35:34 -0300

    test: create search input test

--- src/pages/Dashboard/Dashboard.spec.tsx
+++ src/pages/Dashboard/Dashboard.spec.tsx
@@ -1,6 +1,9 @@
 import { vi } from 'vitest';
-import { botListMock } from '../../tests/__mocks__';
+import userEvent from '@testing-library/user-event';
+import { botListMock, renderWithWrapper } from '../../tests/__mocks__';
 import { IntelligentContacts } from '../../services/intelligentContacts';
+import { FavoritesProvider } from '../../hooks/useFavorites';
+import { Dashboard } from '.';
 
 vi.mock('../../services/intelligentContacts', () => {
   return {
@@ -15,6 +18,13 @@ vi.mock('../../services/intelligentContacts', () => {
   };
 });
 
+const IntersectionObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+}));
+
+vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
+
 describe('Dashboard Page', () => {
   it('should able to load a list of bots', async () => {
     const response = await IntelligentContacts.getBots({
@@ -24,5 +34,31 @@ describe('Dashboard Page', () => {
     });
 
     expect(response.data).toEqual(botListMock);
+  });
+
+  it('should able to search a bot', async () => {
+    vi.mock('../../utils/formatDate', () => {
+      return {
+        formatDate: vi.fn(),
+      };
+    });
+
+    const user = userEvent.setup();
+    const { getByPlaceholderText, findByDisplayValue, queryByDisplayValue } =
+      renderWithWrapper(
+        <FavoritesProvider>
+          <Dashboard />
+        </FavoritesProvider>,
+      );
+    const searchInput = getByPlaceholderText('Search');
+    await user.type(searchInput, 'fake-name');
+    const searchResult = await findByDisplayValue('fake-name');
+
+    expect(searchResult).toBeInTheDocument();
+
+    await user.type(searchInput, 'not-found-name');
+    const notFoundSearch = queryByDisplayValue('not-found-name');
+
+    expect(notFoundSearch).not.toBeInTheDocument();
   });
 });
