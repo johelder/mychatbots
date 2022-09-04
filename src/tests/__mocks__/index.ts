bc840ee2d46817bdcc44f2dc646b012eae1ccff0
johelder <johelder.araujo@pede.ai>
2022-09-04 17:22:25 -0300

    test: create mocks index

---
+++ src/tests/__mocks__/index.ts
@@ -0,0 +1,6 @@
+import { renderWithWrapper } from './renderMock';
+import { botMock } from './botMock';
+import { botDetailMock } from './botDetailMock';
+import { botListMock } from './botListMock';
+
+export { renderWithWrapper, botMock, botDetailMock, botListMock };
