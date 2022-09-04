07a5a607027a806479ab7507972e95d7a461f83f
johelder <johelder.araujo@pede.ai>
2022-09-04 17:22:03 -0300

    test: create botList mock

---
+++ src/tests/__mocks__/botListMock.ts
@@ -0,0 +1,22 @@
+export const botListMock = [
+  {
+    name: 'fake-name',
+    type: 'fake-name',
+    created: 'fake-date',
+  },
+  {
+    name: 'fake-name',
+    type: 'fake-name',
+    created: 'fake-date',
+  },
+  {
+    name: 'fake-name',
+    type: 'fake-name',
+    created: 'fake-date',
+  },
+  {
+    name: 'fake-name',
+    type: 'fake-name',
+    created: 'fake-date',
+  },
+];
