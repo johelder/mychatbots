097176661f4f3b5e61c02e453418298c66c8072b
johelder <johelder.araujo@pede.ai>
2022-09-04 21:53:32 -0300

    feat: adding new components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,16 @@ import { OrganizeListTypeButton } from './OrganizeListTypeButton';
 import BotCard from './BotCard';
 import { Footer } from './Footer';
 import { BotInfoCard } from './BotInfoCard';
+import { Error } from './Error';
+import { Loading } from './Loading';
 
-export { Header, Button, OrganizeListTypeButton, BotCard, Footer, BotInfoCard };
+export {
+  Header,
+  Button,
+  OrganizeListTypeButton,
+  BotCard,
+  Footer,
+  BotInfoCard,
+  Error,
+  Loading,
+};
